fecfc48546c05cf22241fea9bb23a6557c436c3a
Cansu-1907 <cansu.bilal@student.ehb.be>
2021-08-07 21:41:29 +0200

    creating blog object and artikel class, fetch data

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 "use strict";
+
+const blog = {
+  artikelsArray: [],
+  init() {
+    this.getData();
+  },
+  async getData() {
+    await fetch("https://thecrew.cc/news/read.php")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.news);
+        data.news.forEach((element) => {
+          this.artikelsArray.push(
+            new Artikel(
+              element.UUID,
+              element.title,
+              element.content,
+              element.imageURI,
+              element.likes,
+              element.publicationDate
+            )
+          );
+        });
+        console.log(this.artikelsArray);
+      });
+  },
+};
+
+class Artikel {
+  constructor(id, title, content, imageURL, likes, datum) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.imageURL = imageURL;
+    this.likes = likes;
+    this.datum = datum;
+  }
+}
+
+blog.init();
